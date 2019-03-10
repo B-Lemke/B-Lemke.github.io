c2efdc900501f3698530250ef772380c4640c473
Brody Lemke <500161066@fvtc.edu>
2019-03-09 23:29:28 -0600

    Adding images to projects

--- resources/scripts/activeController.js
+++ resources/scripts/activeController.js
@@ -5,9 +5,6 @@ var app = angular.module('myApp',[]);
 app.controller("MenuCtrl", function($scope, $location) {
   $scope.menuClass = function(page) {
     var current = window.location.pathname.split( '/' );
-    current = current[current.length-1];
-    console.log(page + ":");
-    page === current ? console.log("active") : console.log("");
     return page === current ? "active" : "";
   };
 });
