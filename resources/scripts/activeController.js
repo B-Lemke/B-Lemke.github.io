bbab11c1ebd7c9370de2a5364f715425d361c499
Brody Lemke <500161066@fvtc.edu>
2019-03-10 12:20:11 -0500

    Fixed active controller

--- resources/scripts/activeController.js
+++ resources/scripts/activeController.js
@@ -5,8 +5,7 @@ var app = angular.module('myApp',[]);
 app.controller("MenuCtrl", function($scope, $location) {
   $scope.menuClass = function(page) {
     var current = window.location.pathname.split( '/' );
-    console.log(current);
-
+    current = current[current.length-1];
     return page === current ? "active" : "";
   };
 });
