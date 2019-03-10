b01111292d0dfb5ffbd0d4fbbd9aef3b6a6108cc
Brody Lemke <500161066@fvtc.edu>
2019-03-10 12:11:07 -0500

    Debugging change to the active controller on github pages

--- resources/scripts/activeController.js
+++ resources/scripts/activeController.js
@@ -5,8 +5,8 @@ var app = angular.module('myApp',[]);
 app.controller("MenuCtrl", function($scope, $location) {
   $scope.menuClass = function(page) {
     var current = window.location.pathname.split( '/' );
+    console.log(current);
+
     return page === current ? "active" : "";
   };
 });
-
-console.log("Test");
