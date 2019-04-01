d1a0a7d417f5230b429abc73d1de79f0cab7fdc1
Brody Lemke <500161066@fvtc.edu>
2019-03-31 22:15:30 -0500

    Updated index, word cloud on about, and added contact page form.

--- resources/scripts/scrollAnimation.js
+++ resources/scripts/scrollAnimation.js
@@ -19,8 +19,8 @@ $(document).ready(function() {
 function CheckFadeAnimation(){
   $('.fadeInAnimation').each( function(i){
 
-    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
+      var bottom_of_object = $(this).offset().top + 100;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       /* If the object is completely visible in the window, fade it it */
       if( bottom_of_window > bottom_of_object ){
